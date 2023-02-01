edb41085ddebcdb7edcbf0343fcd8d855e8da758
Renato Cabrera <renatocabrerafigueroa@gmail.com>
2023-01-31 22:47:54 -0600

    Logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-        <img src="images/km-logo.png" alt="Imagen"></img>
+        <img src="km-logo.png" alt="Logo"></img>
         </div>
 
         <div className={styles.grid}>
