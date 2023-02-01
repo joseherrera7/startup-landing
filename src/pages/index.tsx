db67459af96135a6cfefb100211a1a7b4344920d
Renato Cabrera <renatocabrerafigueroa@gmail.com>
2023-01-31 22:32:25 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,23 +20,7 @@ export default function Home() {
             Bienvenidos a &nbsp;
             <code className={styles.code}>KindMind</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          
         </div>
 
         <div className={styles.center}>
